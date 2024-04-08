0ab56fd01dff93809ce0dafccce345f6851923b8
Adama Jarju <jarjuadama101@gmail.com>
2024-04-08 14:27:50 +0000

    first psuh for 8-April

---
+++ lib/screens/parent_screens/parent_learning_screen/lesson_nine_video.dart
@@ -0,0 +1,98 @@
+import 'package:fedaafrica/routes/app_routes.dart';
+import 'package:fedaafrica/widgets/custom_bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoNinePlayer extends StatefulWidget {
+  final String videoPath;
+
+  const VideoNinePlayer({Key? key, required this.videoPath})
+      : super(key: key);
+
+  @override
+  _VideoNinePlayerState createState() =>
+      _VideoNinePlayerState();
+}
+
+GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+
+class _VideoNinePlayerState extends State<VideoNinePlayer> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoPath)
+      ..initialize().then((_) {
+        setState(() {});
+      })
+      ..addListener(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, AppRoutes.parentLearningScreen);
+          },
+        ),
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+      bottomNavigationBar: _buildBottomBar(context),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  /// Section Widget
+  Widget _buildBottomBar(BuildContext context) {
+    return CustomBottomBar(
+      onChanged: (BottomBarEnum type) {
+        Navigator.pushNamed(
+            navigatorKey.currentContext!, getCurrentRoute(type));
+      },
+    );
+  }
+
+  ///Handling route based on bottom click actions
+  String getCurrentRoute(BottomBarEnum type) {
+    switch (type) {
+      case BottomBarEnum.Home:
+        return AppRoutes.homeScreenOneScreen;
+      case BottomBarEnum.Task:
+        return AppRoutes.taskScreen;
+      case BottomBarEnum.Analytics:
+        return "/";
+      case BottomBarEnum.Learn:
+        return AppRoutes.parentLearningScreen;
+      default:
+        return AppRoutes.homeScreenOneScreen;
+    }
+  }
+}
---
+++ lib/screens/parent_screens/parent_learning_screen/widgets/lesson_eight_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:fedaafrica/core/app_export.dart';
+
+// ignore: must_be_immutable
+class lessonEightScreen extends StatelessWidget {
+  const lessonEightScreen({Key? key, required String videoPath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the video player screen
+        Navigator.pushNamed(context, AppRoutes.videoEightScreen);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 0),
+                child: Container(
+                  height: 187.v,
+                  width: 144.h,
+                  decoration: AppDecoration.fillGray90004.copyWith(
+                    borderRadius: BorderRadiusStyle.roundedBorder25,
+                  ),
+                  child: CustomImageView(
+                    imagePath: ImageConstant.imgRectangle2031,
+                    height: 187.v,
+                    width: 144.h,
+                    radius: BorderRadius.circular(
+                      25.h,
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 18.h,
+                  top: 2.v,
+                  bottom: 28.v,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 179.h,
+                      child: Text(
+                        "How to Buy Stocks and Bonds (A Complete Guide)",
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: theme.textTheme.headlineMedium,
+                      ),
+                    ),
+                    SizedBox(height: 5.v),
+                    Row(
+                      children: [
+                        CustomImageView(
+                          imagePath: ImageConstant.imgCalendar,
+                          height: 24.adaptSize,
+                          width: 24.adaptSize,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            left: 7.h,
+                            top: 6.v,
+                            bottom: 3.v,
+                          ),
+                          child: Text(
+                            "25/4/2024 - 8:00 PM",
+                            style: CustomTextStyles.bodySmallBluegray400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12.v),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "By ",
+                            style: CustomTextStyles.bodySmallff878787,
+                          ),
+                          TextSpan(
+                            text: "Just Ask Ana",
+                            style: CustomTextStyles.bodySmallff4e65c2,
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
